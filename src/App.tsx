51f10e0e5ff2b9bbb3df967669361e13d5bf0b2e
pappferencgabor <p.feco2005@gmail.com>
2024-11-21 08:59:16 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
 import './app.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 import { fetchAdvice } from './api/api'
 import { adviceType } from './type/type'
 
 function App() {
 	const [advice, setAdvice] = useState<adviceType>()
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		const getAdvice = async() => {
 			const adviceData = await fetchAdvice()
 			setAdvice(adviceData)
+			setIsLoading(false)
 		}
 		getAdvice()
 	}, [])
 
+	const handleSubmit = async(e:FormEvent) => {
+		e.preventDefault()
+		const adviceData = await fetchAdvice()
+		setAdvice(adviceData)
+		setIsLoading(false)
+	}
+
 	return (
 		<>
-			<div className='container'>
+			<form className='container' onSubmit={handleSubmit}>
 				<p className='advice-num'>Advice #{advice?.id}</p>
 
-				<h2 className='advice-text'><q>{advice?.advice}</q></h2>
+				<h2 className='advice-text'>
+					{ 
+						isLoading ? 'Loading' : <q>{advice?.advice}</q>
+					}
+				</h2>
 
-				<hr/>
+				<img src="public/pattern-divider-mobile.svg" alt="" className='dividerMobile'/>
+				<img src="public/pattern-divider-desktop.svg" alt="" className='dividerDesktop'/>
+				{/* <hr/> */}
 
-				<button>
+				<button type='submit'>
 					<img src="public/icon-dice.svg" alt="" />
 				</button>
-			</div>
+			</form>
 		</>
 	)
 }
